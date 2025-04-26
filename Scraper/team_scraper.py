ad93a572bf23efc2dba27da0a3eaecc7f4c633ad
ZaneLesley <zanelesley@outlook.com>
2025-04-26 14:11:35 -0500

    Feat(Teams): Finished being able to get all teams from major regions

--- Scraper/team_scraper.py
+++ Scraper/team_scraper.py
@@ -1,20 +1,39 @@
 from mwrogue.esports_client import EsportsClient
+from rich import print
+import time
 
 site = EsportsClient('lol')
 
-teams = {"LCK":[]}
+teams = {
+    "Europe League Championship Series": [],
+    "League of Legends Championship Series": [],
+    "LoL Champions Korea": [],
+    "League of Legends Championship of The Americas North": [],
+    "League of Legends Championship of The Americas South": [],
+    "North America League Championship Series": [],
+    "Tencent LoL Pro League": [],
+}
 
-for tournament_result_row in site.cargo_client.query(
-        limit="max",
-        tables="Tournaments=T, TournamentResults=TR",
-        fields="T.Name, TR.Event, TR.Team",
-        where="T.League = 'LoL Champions Korea' "
-              "AND T.Name != 'None' "
-              "AND TR.Team IS NOT NULL "
-              "AND T.Name NOT LIKE '%Promotion%'",
-        join_on="T.Name = TR.Event"
-):
-    if (tournament_result_row["Team"] in teams["LCK"]):
-        continue
-    teams["LCK"].append(tournament_result_row["Team"])
+print("[blue]Getting Teams")
+
+for league in teams:
+    print("[yellow]Waiting 3 seconds to avoid rate limit")
+    time.sleep(3)
+
+    for tournament_result_row in site.cargo_client.query(
+            limit="max",
+            tables="Tournaments=T, TournamentResults=TR",
+            fields="T.Name, TR.Event, TR.Team",
+            where=f"T.League = '{league}'"
+                  "AND T.Name != 'None' "
+                  "AND TR.Team IS NOT NULL "
+                  "AND T.Name NOT LIKE '%Promotion%'",
+            join_on="T.Name = TR.Event"
+    ):
+        if (tournament_result_row["Team"] in teams[league]):
+            continue
+        teams[league].append(tournament_result_row["Team"])
+
+    print(f"[green]Finished {league}")
+
 print(teams)

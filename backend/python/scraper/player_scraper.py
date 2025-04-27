b647552ea3ff647e88388ca073f5f9989d3ee84e
ZaneLesley <zanelesley@outlook.com>
2025-04-27 12:45:58 -0500

    feat(scraper): Finished scrapping initial players for each season

--- backend/python/scraper/player_scraper.py
+++ backend/python/scraper/player_scraper.py
@@ -5,30 +5,38 @@ import time
 site = EsportsClient('lol')
 
 leagues = {
-    "Europe League Championship Series": [],
-    "League of Legends Championship Series": [],
-    "LoL Champions Korea": [],
-    "League of Legends Championship of The Americas North": [],
-    "League of Legends Championship of The Americas South": [],
-    "North America League Championship Series": [],
-    "Tencent LoL Pro League": [],
+    "Europe League Championship Series": {},
+    "League of Legends Championship Series": {},
+    "LoL Champions Korea": {},
+    "League of Legends Championship of The Americas North": {},
+    "League of Legends Championship of The Americas South": {},
+    "North America League Championship Series": {},
+    "Tencent LoL Pro League": {},
 }
 
 for league in leagues:
     time.sleep(3)
-
-    for tournament_result_row in site.cargo_client.query(
-            limit=500,
-            tables="Tournaments=T, TournamentResults=TR",
-            fields="T.Name, TR.Event, TR.Team",
+    for player_tournament_results in site.cargo_client.query(
+            tables="Tournaments=T, TournamentPlayers=TP",
+            fields="T.Name, TP.OverviewPage, TP.Team, TP.Player, TP.Role, TP.Flag, TP.Link",
             where=f"T.League = '{league}'"
-                  "AND T.Name != 'None' "
-                  "AND TR.Team IS NOT NULL "
+                  "AND TP.Role != 'Coach'"
                   "AND T.Name NOT LIKE '%Promotion%'",
-            join_on="T.Name = TR.Event"
+            join_on="T.OverviewPage=TP.OverviewPage",
     ):
-        if tournament_result_row["Team"] in leagues[league]:
-            continue
-        leagues[league].append(tournament_result_row["Team"])
 
-print(json.dumps(leagues))
+        team_name = player_tournament_results["Team"]
+        event_name = player_tournament_results["OverviewPage"]
+        print(event_name)
+        if event_name not in leagues[league]:
+            leagues[league][event_name] = {}
+
+        if team_name not in leagues[league][event_name]:
+            leagues[league][event_name][team_name] = {
+                "Players": [],
+            }
+
+        leagues[league][event_name][team_name]["Players"].append(player_tournament_results)
+
+with open("data.json", "w") as json_file:
+    json.dump(leagues, json_file, indent=4)

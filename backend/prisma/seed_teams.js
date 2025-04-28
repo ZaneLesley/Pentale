fcab4ea5a3627805171bfee2b829f7d6ebc4b1cf
ZaneLesley <zanelesley@outlook.com>
2025-04-28 14:05:26 -0500

    fix(seed_teams): All major regions added, and added batching for the query

--- backend/prisma/seed_teams.js
+++ backend/prisma/seed_teams.js
@@ -1,5 +1,5 @@
-const { PrismaClient } = require('../generated/prisma');
-const { runScraperFile } = require('../python/scraper_controller');
+const {PrismaClient} = require('../generated/prisma');
+const {runScraperFile} = require('../python/scraper_controller');
 
 const prisma = new PrismaClient();
 
@@ -9,19 +9,25 @@ const abbreviations = {
     "LoL Champions Korea": "LCK",
     "League of Legends Championship of The Americas North": "LTA North",
     "League of Legends Championship of The Americas South": "LTA South",
-    "North America League Championship Series": "NALCS",
+    "North America League Championship Series": "LCS",
     "Tencent LoL Pro League": "LPL",
-}
+    "LoL The Champions": "LCK",
+    "LoL EMEA Championship": "LEC",
+};
 
 async function main() {
     try {
+        await prisma.playerPerSplit.deleteMany({});
+        await prisma.$executeRaw`TRUNCATE TABLE "PlayerPerSplit" RESTART IDENTITY CASCADE;`;
+        await prisma.player.deleteMany({});
+        await prisma.$executeRaw`TRUNCATE TABLE "Player" RESTART IDENTITY CASCADE;`;
         await prisma.team.deleteMany({});
         await prisma.$executeRaw`TRUNCATE TABLE "Team" RESTART IDENTITY CASCADE;`;
 
         const jsonFile = 'team_scraper.json';
-        
+
         const result = await runScraperFile('team_scraper.py', [jsonFile]);
-        const teamsByLeague = result[jsonFile]
+        const teamsByLeague = result[jsonFile];
 
         for (const [league, teams] of Object.entries(teamsByLeague)) {
             for (const team of teams) {
--- backend/python/scraper/team_scraper.py
+++ backend/python/scraper/team_scraper.py
@@ -7,29 +7,42 @@ site = EsportsClient('lol')
 leagues = {
     "Europe League Championship Series": [],
     "League of Legends Championship Series": [],
+    "LoL EMEA Championship": [],
     "LoL Champions Korea": [],
+    "LoL The Champions": [],
     "League of Legends Championship of The Americas North": [],
     "League of Legends Championship of The Americas South": [],
     "North America League Championship Series": [],
     "Tencent LoL Pro League": [],
 }
 
-for league in leagues:
-    time.sleep(3)
 
-    for tournament_result_row in site.cargo_client.query(
-            limit=500,
+
+for league in leagues:
+    batch_size = 500
+    offset = 0
+    while True:
+        time.sleep(1.5)
+
+        batch = site.cargo_client.query(
+            limit=batch_size,
+            offset=offset,
             tables="Tournaments=T, TournamentResults=TR",
             fields="T.Name, TR.Event, TR.Team",
-            where=f"T.League = '{league}'"
+            where=f"T.League = '{league}' "
                   "AND T.Name != 'None' "
                   "AND TR.Team IS NOT NULL "
                   "AND T.Name NOT LIKE '%Promotion%'",
             join_on="T.Name = TR.Event"
-    ):
-        if tournament_result_row["Team"] in leagues[league]:
-            continue
-        leagues[league].append(tournament_result_row["Team"])
+    )
+        if not batch:
+            break
+
+        for tournament_result_row in batch:
+            if tournament_result_row["Team"] in leagues[league]:
+                continue
+            leagues[league].append(tournament_result_row["Team"])
+        offset += batch_size
 
 with open("team_scraper.json", "w") as json_file:
     json.dump(leagues, json_file, indent=4)

a2c6d32a52dc28f4be336e859422396a1dcba8fe
ZaneLesley <zanelesley@outlook.com>
2025-05-03 00:35:45 -0500

    feat(express-session): setup express session for game cookie logging

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,31 @@
-require('dotenv').config();
 const express = require('express');
-const app = express();
+const session = require('express-session');
 const cors = require('cors');
+require('dotenv').config();
 
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Middleware
 app.locals.encodeURIComponenet = encodeURIComponent;
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+app.use(express.json());
+
+// Session Middleware
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,                          // Change if using HTTPS
+        //httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24             // 24 hours
+    }
+}));
 
 // Log all routes requests to console
 app.use((req, res, next) => {
@@ -12,20 +33,20 @@ app.use((req, res, next) => {
     next();
 });
 
-const PORT = process.env.PORT || 8080;
 
-app.get("/",  (req, res) => {
+// Router Redirect
+app.get("/", (req, res) => {
     res.redirect("/api");
-})
+});
 
 
-// Router
+// Routers
 const apiRouter = require('../backend/routes/apiRouter');
 app.use("/api", apiRouter);
 
 const gameRouter = require('../backend/routes/gameRouter');
-app.use("/game", gameRouter)
+app.use("/game", gameRouter);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
-})
+});

9590e18335212a89e1abd2955f311aa63006de60
ZaneLesley <zanelesley@outlook.com>
2025-05-06 20:02:10 -0500

    feat(fetch): wrote out how I'm going to fetch data. I'll be writing most of them manually instead of adding a function on purpose to get the idea behind it

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,23 @@
+import {useEffect} from "react";
+
 function LandingPage() {
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/game');
+                const data = await response.json();
+                console.log(data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData()
+    }, []);
+
     return (
         <div className="landing-page">Stuff</div>
-    )
+    );
 }
 
 export default LandingPage;

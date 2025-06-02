fc55716d15d52e24723d64707141649a185af56c
zanelesley <zanelesley@outlook.com>
2025-06-02 08:12:24 -0500

    fix(db seed): fixed an issue where the player_stats_scraper.py saved username as the Link, leading to not being able to find that record in the database (since we use the redirect name), this is fixed by cross-referencing the link with the player ID

---
+++ backend/prisma/migrations/20250602125908_records_to_wins_and_losses/migration.sql
@@ -0,0 +1,10 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `record` on the `Player` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Player" DROP COLUMN "record",
+ADD COLUMN     "losses" INTEGER,
+ADD COLUMN     "wins" INTEGER;
--- backend/prisma/seed_stats.js
+++ backend/prisma/seed_stats.js
@@ -1,10 +1,11 @@
 const {runStatsFile} = require('../python/scraper_controller');
 const fs = require("fs");
-const {PrismaClient} = require('../generated/prisma')
+const {PrismaClient} = require('../generated/prisma');
 
 const prisma = new PrismaClient();
-async function main(){
-    await runStatsFile();
+
+async function main() {
+    // await runStatsFile();
 
     const data = JSON.parse(fs.readFileSync('temp_player_stats.json', 'utf8'));
 
@@ -12,27 +13,55 @@ async function main(){
         for (const row of data) {
             for (const [player, stats] of Object.entries(row)) {
                 const cspm = stats["Gamelength Number"] === 0 ? 0 : Math.round((stats.CS / stats["Gamelength Number"]) * 10) / 10;
-                await prisma.player.update({
-                    data: {
-                        kills: stats.Kills,
-                        assists: stats.Assists,
-                        deaths: stats.Deaths,
-                        cspm: cspm,
-                        record: `${stats.Win} - ${stats.Loss}`
-                    },
+                const playerRecord = await prisma.player.findFirst({
                     where: {
-                        name: player
+                        // This is the link attribute, need to compare it to the playerId and update it there
+                        PlayerPerSplit: {
+                            some: {
+                                link: player
+                            }
+                        }
+                    },
+                    select: {
+                        id: true
                     }
-                })
+                });
+                if (playerRecord) {
+                    await prisma.player.update({
+                        where: {
+                            id: playerRecord.id
+                        },
+                        data: {
+                            kills: {
+                                increment: stats.Kills
+                            },
+                            assists: {
+                                increment: stats.Assists
+                            },
+                            deaths: {
+                                increment: stats.Deaths
+                            },
+                            cspm: {
+                                increment: cspm
+                            },
+                            wins : {
+                                increment: stats.Win
+                            },
+                            losses : {
+                                increment: stats.Loss
+                            }
+                        }
+                    });
+                }
             }
         }
     } catch (error) {
         console.log(error);
     } finally {
-        prisma.$disconnect()
-        console.log("records added")
+        prisma.$disconnect();
+        console.log("records added");
     }
 
 }
 
-main()
+main();

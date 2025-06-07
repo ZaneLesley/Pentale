d1e01987983dc8c93d54b2417e3d7272a8bca917
zanelesley <zanelesley@outlook.com>
2025-06-06 20:24:38 -0500

    style(modal)

--- frontend/src/pages/LandingPage/Game/WinModal.jsx
+++ frontend/src/pages/LandingPage/Game/WinModal.jsx
@@ -1,6 +1,7 @@
 import {Box, Modal} from "@mui/material";
 import {useState, useEffect} from "react";
 import {fetchCorrectGuess, generateGame} from "../../../api/game";
+import styles from './WinModal.module.css'
 
 export default function WinModal({setPlayers, state, setShowModal}) {
     const [open, setOpen] = useState(true);
@@ -28,28 +29,13 @@ export default function WinModal({setPlayers, state, setShowModal}) {
         fetchGuess();
     }, []);
 
-
-    const style = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'gray',
-        border: '2px solid #000',
-        boxShadow: 24,
-        pt: 2,
-        px: 4,
-        pb: 3,
-    };
-
     return (
         <>
             <Modal
                 open={open}
                 onClose={handleClose}
             >
-                <Box sx={{...style, width: 200}}>
+                <Box className={styles.container}>
                     <div>{`You ${state.status}!`}</div>
                     <div>{`Number of guesses: ${state.numGuesses}`}</div>
                     <div>{correctGuess ? `Correct Guess: ${correctGuess}` : 'Loading...'}</div>
